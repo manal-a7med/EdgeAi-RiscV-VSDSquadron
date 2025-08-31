a6b27c2e6c6e291020157ae8c9bda85556e4edc9
Manal Ahmed <manalahmedmatheen@gmail.com>
2025-08-31 12:11:55 +0530

    added files

---
+++ header files/scaler.h
@@ -0,0 +1,9 @@
+#define NUM_FEATURES 2
+
+double scaler_mean[NUM_FEATURES] = {
+    38.1266666667, 69583.3333333333
+};
+
+double scaler_std[NUM_FEATURES] = {
+    10.0977203148, 34490.9126518211
+};
